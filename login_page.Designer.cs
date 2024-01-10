e6063eb5ba2a4a15814002cd4ac9ac3ee99b4777
UmarSidiki <siddiquiumar0007@gmail.com>
2024-01-11 02:27:48 +0500

    Add project files.

---
+++ login_page.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Multi_Tool
+{
+    partial class login_page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Signup = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(63, 122);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(245, 22);
+            this.username.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(63, 195);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(245, 22);
+            this.password.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.LawnGreen;
+            this.login.Location = new System.Drawing.Point(136, 261);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(105, 33);
+            this.login.TabIndex = 5;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "New user? Create a new account.";
+            // 
+            // Signup
+            // 
+            this.Signup.Location = new System.Drawing.Point(136, 356);
+            this.Signup.Name = "Signup";
+            this.Signup.Size = new System.Drawing.Size(105, 33);
+            this.Signup.TabIndex = 7;
+            this.Signup.Text = "Signup";
+            this.Signup.UseVisualStyleBackColor = true;
+            this.Signup.Click += new System.EventHandler(this.Signup_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 478);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(325, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "This Project is Created by: Muhammad Umar Siddiqui";
+            // 
+            // login_page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 503);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Signup);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "login_page";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multi_Tool Project";
+            this.Load += new System.EventHandler(this.login_page_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Signup;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ login_page.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_Tool
+{
+    public partial class login_page : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Muhammad Umar\\Documents\\multi_tool.mdf\";Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd;
+        public login_page()
+        {
+            InitializeComponent();
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+        }
+
+        private void login_page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Signup_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            signup_page signup_page = new signup_page();
+            signup_page.Show();
+        }
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+        }
+    }
+}
